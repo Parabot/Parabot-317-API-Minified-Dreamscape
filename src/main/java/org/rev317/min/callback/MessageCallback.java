df9b0f15109400155a505deb72fc7e9007472c4b
Emma Stone <capslock_donater@hotmail.com>
2017-08-29 13:48:11 +0100

    [TASK] Removed old method

--- src/main/java/org/rev317/min/callback/MessageCallback.java
+++ src/main/java/org/rev317/min/callback/MessageCallback.java
@@ -5,11 +5,6 @@ import org.rev317.min.script.ScriptEngine;
 
 public class MessageCallback {
 
-    public static final void messageListenerHook(int type, String name, String message) {
-        final MessageEvent messageEvent = new MessageEvent(type, name, message);
-        ScriptEngine.getInstance().dispatch(messageEvent);
-    }
-
     public static final void messageListenerHook(int type, String name, String message, String title) {
         final MessageEvent messageEvent = new MessageEvent(type, name, message, title);
         ScriptEngine.getInstance().dispatch(messageEvent);
