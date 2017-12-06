16723ed0a99b58cd6f2fc767bea3c5a960fb1f4c
Emmastone <capslock_donater@hotmail.com>
2017-12-06 19:06:34 +0000

    [BUGFIX] Increase experience to 150
    Dreamscape's max level is 150 not 120.

--- src/main/java/org/rev317/min/api/methods/Skill.java
+++ src/main/java/org/rev317/min/api/methods/Skill.java
@@ -6,25 +6,52 @@ import org.rev317.min.Loader;
  * @author Dane, JKetelaar
  */
 public enum Skill {
+    ATTACK,
+    DEFENSE,
+    STRENGTH,
+    HITPOINTS,
+    RANGE,
+    PRAYER,
+    MAGIC,
+    COOKING,
+    WOODCUTTING,
+    FLETCHING,
+    FISHING,
+    FIREMAKING,
+    CRAFTING,
+    SMITHING,
+    MINING,
+    HERBLORE,
+    AGILITY,
+    THIEVING,
+    SLAYER,
+    FARMING,
+    RUNECRAFTING,
+    CONSTRUCTION,
+    HUNTER,
+    SUMMONING,
+    DUNGEONEERING;
 
-    ATTACK, DEFENSE, STRENGTH, HITPOINTS, RANGE, PRAYER, MAGIC, COOKING, WOODCUTTING, FLETCHING, FISHING, FIREMAKING, CRAFTING, SMITHING, MINING, HERBLORE, AGILITY, THIEVING, SLAYER, FARMING, RUNECRAFTING, CONSTRUCTION, HUNTER, SUMMONING, DUNGEONEERING;
-
-    private static final int[] EXPERIENCE = { 0, 0, 83, 174, 276, 388, 512,
-            650, 801, 969, 1154, 1358, 1584, 1833, 2107, 2411, 2746, 3115,
-            3523, 3973, 4470, 5018, 5624, 6291, 7028, 7842, 8740, 9730, 10824,
-            12031, 13363, 14833, 16456, 18247, 20224, 22406, 24815, 27473,
-            30408, 33648, 37224, 41171, 45529, 50339, 55649, 61512, 67983,
-            75127, 83014, 91721, 101333, 111945, 123660, 136594, 150872,
-            166636, 184040, 203254, 224466, 247886, 273742, 302288, 333804,
-            368599, 407015, 449428, 496254, 547953, 605032, 668051, 737627,
-            814445, 899257, 992895, 1096278, 1210421, 1336443, 1475581,
-            1629200, 1798808, 1986068, 2192818, 2421087, 2673114, 2951373,
-            3258594, 3597792, 3972294, 4385776, 4842295, 5346332, 5902831,
-            6517253, 7195629, 7944614, 8771558, 9684577, 10692629, 11805606,
-            13034431, 14391160, 15889109, 17542976, 19368992, 21385073,
-            23611006, 26068632, 28782069, 31777943, 35085654, 38737661,
-            42769801, 47221641, 52136869, 57563718, 63555443, 70170840,
-            77474828, 85539082, 94442737, 104273167 };
+    private static final long[] EXPERIENCE = {
+            0, 0, 83, 174, 276, 388, 512, 650, 801, 969, 1154, 1358, 1584,
+            1833, 2107, 2411, 2746, 3115, 3523, 3973, 4470, 5018, 5624, 6291,
+            7028, 7842, 8740, 9730, 10824, 12031, 13363, 14833, 16456, 18247,
+            20224, 22406, 24815, 27473, 30408, 33648, 37224, 41171, 45529, 50339,
+            55649, 61512, 67983, 75127, 83014, 91721, 101333, 111945, 123660, 136594,
+            150872, 166636, 184040, 203254, 224466, 247886, 273742, 302288, 333804,
+            368599, 407015, 449428, 496254, 547953, 605032, 668051, 737627, 814445,
+            899257, 992895, 1096278, 1210421, 1336443, 1475581, 1629200, 1798808, 1986068,
+            2192818, 2421087, 2673114, 2951373, 3258594, 3597792, 3972294, 4385776,
+            4842295, 5346332, 5902831, 6517253, 7195629, 7944614, 8771558, 9684577,
+            10692629, 11805606, 13034431, 14391160, 15889109, 17542976, 19368992,
+            21385073, 23611006, 26068632, 28782069, 31777943, 35085654, 38737661,
+            42769801, 47221641, 52136869, 57563718, 63555443, 70170840, 77474828,
+            85539082, 94442737, 104273167, 115126838, 127110260, 140341028,
+            154948977, 171077457, 188884740, 208545572, 230252886, 254219702,
+            280681209, 309897078, 342154009, 377768545, 417090179, 460504778,
+            508438379, 561361362, 619793069, 684306901, 755535943, 834179178,
+            921008346, 1016875516, 1122721449, 1239584831, 1368612462, 1511070513,
+            1668356950, 1842015252, 2033749558 };
 
     /**
      * Returns the experience of the provided skill.
@@ -66,8 +93,8 @@ public enum Skill {
      *
      * @return the experience at the provided level.
      */
-    public static final int getExperienceByLevel(int level) {
-        if (level > 99 || level < 1) {
+    public static final long getExperienceByLevel(int level) {
+        if (level > 150 || level < 1) {
             return 0;
         }
 
@@ -98,9 +125,9 @@ public enum Skill {
      *
      * @return the remaining experience.
      */
-    public static final int getRemainingExperience(int index) {
+    public static final long getRemainingExperience(int index) {
         int level = getLevelByExperience(getCurrentExperience(index));
-        if (level >= 99 || level < 1) {
+        if (level > 150 || level < 1) {
             return 0;
         }
 
@@ -117,7 +144,7 @@ public enum Skill {
     public static final int getPercentToNextLevel(int index) {
         int currentLevel = getLevelByExperience(getCurrentExperience(index));
         int nextLevel    = currentLevel + 1;
-        if (currentLevel == 99 || nextLevel > 99 || currentLevel < 1
+        if (currentLevel == 150 || nextLevel > 150 || currentLevel < 1
                 || nextLevel < 1) {
             return 0;
         }
@@ -164,7 +191,7 @@ public enum Skill {
     /**
      * Returns the remaining experience until the next level.
      */
-    public final int getRemaining() {
+    public final long getRemaining() {
         return Skill.getRemainingExperience(this.ordinal());
     }
 
